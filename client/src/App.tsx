c1ee69b9dbcd37e9993eb21fe318b9a79a7724d1
Roland Mackintosh <rolandmackintosh@outlook.com>
2024-08-06 11:44:49 -0700

    routes work

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,35 +1,45 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
+
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+const Foo = () => <h2>Foo Page</h2>;
+const Bar = () => <h2>Bar Page</h2>;
+const Baz = () => <h2>Baz Page</h2>;
+
+const routesOptions = {wordRating: "/wordRating", photoUpload: "/photoUpload", photoSetPreview: "/photoSetPreview", ankiDeckDownload: "/ankiDeckDownload"}
+
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
+    <Router>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <nav>
+          <ul>
+          <li>
+              <Link to={routesOptions.wordRating}>Word Rating</Link>
+            </li>
+            <li>
+              <Link to={routesOptions.photoSetPreview}>Photo Set Preview</Link>
+            </li>
+            <li>
+              <Link to={routesOptions.photoUpload}>Photo Upload</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Routes>
+          <Route path={routesOptions.wordRating} element={<Foo />} />
+          <Route path={routesOptions.photoSetPreview} element={<Bar />} />
+          <Route path={routesOptions.photoUpload} element={<Baz />} />
+        </Routes>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </Router>
+  );
 }
 
 export default App
